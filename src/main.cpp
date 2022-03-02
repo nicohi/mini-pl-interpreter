08a197bd8828298e84c3d205f8b1deea7942ebc5
Nicolas Hiillos <nicolashiillos@gmail.com>
2022-03-02 19:12:45 +0200

    Implement REPL stub

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,22 @@
 
 using namespace std;
 
+static void interpret(string l) {
+  cout << "Interpreting line: \"" + l + "\"" << std::endl;
+}
+
+static void repl() {
+  string line;
+  for (;;) {
+    if (cin.eof())
+      break;
+    cout << "> ";
+    cin >> line;
+    interpret(line);
+  }
+}
+
 int main(int argc, char *argv[]) {
-  cout << "Hello mini-pl" << std::endl;
+  repl();
   return 0;
 }
